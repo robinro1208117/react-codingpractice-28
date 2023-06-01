9409c134476e1c32451cbddad49f9f4fb3f308f5
robinro1208117 <robinro1208117@gmail.com>
2023-06-01 10:55:20 +0000

    first commit

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import Header from '../Header/index'
+
+const Cart = () => (
+  <div>
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png "
+      alt="cart"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import Header from '../Header/index'
+
+const Product = () => (
+  <div>
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+    />
+  </div>
+)
+
+export default Product
